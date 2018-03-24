3b1304711dae17da7c1b37b3f301eb605a6c9e5f
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-03-24 15:11:26 -0500

    fixed the loop
    updated that loop

--- ToyData.R
+++ ToyData.R
@@ -1,16 +1,15 @@
 toydata<-data.frame(rep(1,30), sample(1:100, 30, replace=T), sample(c(0,1), 30, replace=T))
 colnames(toydata)<-c("DocIDi", "DocIDj", "Choose")
-toydata
 table(toydata$DocIDj)
 
 lambdai<-runif(30)
 ?sample
 bradleyterry<-function(a,b,lambdai,dataset){
-  sum<-0
+  summationterm<-0
   for (i in nrow(dataset$DocIDj)){
-    sum<-sum+(1/(lambdai[i]+dataset$DocIDj))
+    summationterm<-summationterm+(1/(lambdai[i]+dataset$DocIDj[i]))
     }
-    output<-(a-1+sum(dataset$Choose))/(b+sum)
+    output<-(a-1+sum(dataset$Choose))/(b+summationterm)
     return(output)
 }
 
